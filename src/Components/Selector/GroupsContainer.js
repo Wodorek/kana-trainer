deb04eed7afe7c2f8b3f13ee14a5f797262505cc
Wodorek <58212786+Wodorek@users.noreply.github.com>
2020-11-17 19:53:56 +0100

    StateLifted
    State lifted, need to reiplement check/ uncheck all boxes again

--- src/Components/Selector/GroupsContainer.js
+++ src/Components/Selector/GroupsContainer.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { after, cloneDeep } from 'lodash';
 
 import { dictionary } from '../../InternalData/dictionary';
 import SelectorGroup from './SelectorGroup';
@@ -6,20 +7,71 @@ import SelectorGroup from './SelectorGroup';
 import './GroupsContainer.css';
 
 const GroupsContainer = () => {
-  const groups = Object.entries(dictionary);
+  const [groups, setGroups] = useState();
 
-  const logAll = () => {
-    console.log(groups);
+  console.log(groups);
+
+  useEffect(() => {
+    for (let [key] of Object.entries(dictionary)) {
+      for (let [, values] of Object.entries(dictionary[key])) {
+        values.checked = false;
+      }
+    }
+    setGroups(dictionary);
+  }, []);
+
+  const checkAllHandler = () => {};
+
+  const uncheckAllHandler = (kanaType) => {
+    const allUnchecked = Object(groups[kanaType]);
   };
 
-  return (
-    <div className="groupsContainer">
-      {groups.map((e) => {
-        return <SelectorGroup key={e[0]} groupName={e[0]} items={e[1]} />;
-      })}
-      <button onClick={logAll}>RENDER</button>
-    </div>
-  );
+  const checkHandler = (kanaType, group) => {
+    const afterSelection = cloneDeep(groups);
+    afterSelection[kanaType][group].checked = !afterSelection[kanaType][group]
+      .checked;
+    setGroups(afterSelection);
+  };
+
+  const showSelectedHandler = () => {
+    const selected = [];
+    for (let kana in groups) {
+      for (let group in groups[kana]) {
+        if (groups[kana][group].checked === true) {
+          selected.push(group);
+        }
+      }
+    }
+    console.log(selected);
+  };
+
+  let content;
+
+  if (!groups) {
+    content = <p>Loading</p>;
+  } else {
+    content = (
+      <>
+        <div className="groupsContainer">
+          {Object.keys(groups).map((e) => {
+            return (
+              <SelectorGroup
+                checkOne={checkHandler}
+                checkAll={checkAllHandler}
+                uncheckAll={uncheckAllHandler}
+                key={e}
+                groupName={e}
+                items={groups[e]}
+              />
+            );
+          })}
+        </div>
+        <button onClick={showSelectedHandler}>Show</button>
+      </>
+    );
+  }
+
+  return content;
 };
 
 export default GroupsContainer;
--- src/Components/Selector/SelectorGroup.js
+++ src/Components/Selector/SelectorGroup.js
@@ -4,60 +4,24 @@ import Selector from './Selector';
 const SelectorGroup = (props) => {
   //pass the state to the parent
 
-  const { items } = props;
-
-  const [groups, setGroups] = useState([]);
-
-  useEffect(() => {
-    const groups = Object.keys(items).map((el) => {
-      return { name: el, checked: false };
-    });
-    setGroups([...groups]);
-  }, [items]);
-
-  const checkOneHandler = (id) => {
-    const newGroups = groups.map((group) => {
-      if (group.name === id) {
-        group.checked = !group.checked;
-        return group;
-      }
-      return group;
-    });
-    setGroups(newGroups);
-  };
-
-  const checkAllHandler = () => {
-    const newGroups = groups.map((group) => {
-      group.checked = true;
-      return group;
-    });
-    setGroups(newGroups);
-  };
-
-  const uncheckAllHandler = () => {
-    const newGroups = groups.map((group) => {
-      group.checked = false;
-      return group;
-    });
-    setGroups(newGroups);
-  };
+  const forItemsIteration = Object.keys(props.items);
 
   return (
     <div>
       <h1>{props.groupName}</h1>
-      {groups.map((group) => {
+      {forItemsIteration.map((group) => {
         return (
           <Selector
-            check={checkOneHandler}
-            ticked={group.checked}
-            kana={Object.keys(items[group.name])[0]}
-            key={group.name}
-            group={group.name}
+            check={() => props.checkOne(props.groupName, group)}
+            ticked={props.items[group].checked}
+            kana={Object.keys(props.items[group].characters)[0]}
+            key={group}
+            group={group}
           />
         );
       })}
-      <button onClick={checkAllHandler}>Check All</button>
-      <button onClick={uncheckAllHandler}>Uncheck All</button>
+      <button onClick={props.checkAll}>check</button>
+      <button onClick={() => props.uncheckAll(props.groupName)}>uncheck</button>
     </div>
   );
 };
